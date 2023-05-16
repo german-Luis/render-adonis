d1e47f900aa9f53a5691e1ab980065e431836bf8
germancitom <130400083+germancitom@users.noreply.github.com>
2023-05-15 18:38:08 -0600

    ✍️

--- app/Controllers/Http/HomeController.js
+++ app/Controllers/Http/HomeController.js
@@ -1,11 +1,23 @@
+const { Octokit } = require("@octokit/rest");
+
+
+
 class HomeController {
-    async index ({ auth, view }) {
-      const user = await auth.getUser()
-  
-      return view.render('welcome', {
-        user: user || null
-      })
-    }
+  async index({ auth, view }) {
+    const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN || '', // Usando process.env para acceder a la variable de entorno GITHUB_TOKEN
+    });
+    const user = await auth.getUser()
+
+    return view.render('welcome', {
+      user: user || null
+    })
   }
-  
-  module.exports = HomeController
+
+
+
+}
+
+
+
+module.exports = HomeController
