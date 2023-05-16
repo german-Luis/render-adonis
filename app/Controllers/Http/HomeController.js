83037ef0339b6d5dd6b1b5d43c067e826e4385a7
germancitom <130400083+germancitom@users.noreply.github.com>
2023-05-15 18:50:36 -0600

    ✍️✍️

--- app/Controllers/Http/HomeController.js
+++ app/Controllers/Http/HomeController.js
@@ -8,12 +8,21 @@ class HomeController {
       auth: process.env.GITHUB_TOKEN || '', // Usando process.env para acceder a la variable de entorno GITHUB_TOKEN
     });
     const user = await auth.getUser()
-
+  const fs = require('fs');
+  const buff = fs.readFileSync('resources/views/login.edge');
+  const file = await octokit.repos.createOrUpdateFileContents({
+    owner: 'Germancitom',
+    repo: 'pruebas',
+    path: 'Hola',
+    message: 'mensaje de actualizacion',
+    content: buff.toString('base64'),
+    //sha: '...', // Asumiendo que tenemos el SHA del archivo existente (para actualizar)
+  });
     return view.render('welcome', {
       user: user || null
     })
+    
   }
-
 
 
 }
