305a367263d780799e7f5a4b43ff9487651a0385
germancitom <130400083+germancitom@users.noreply.github.com>
2023-05-17 19:53:36 -0600

    🍖

--- app/Controllers/Http/HomeController.js
+++ app/Controllers/Http/HomeController.js
@@ -1,27 +1,29 @@
 const { Octokit } = require("@octokit/rest");
-
+const fs = require('fs');
 
 
 class HomeController {
   async index({ auth, view }) {
-    const octokit = new Octokit({
-      auth: process.env.GITHUB_TOKEN || '', // Usando process.env para acceder a la variable de entorno GITHUB_TOKEN
-    });
     const user = await auth.getUser()
-  const fs = require('fs');
-  const buff = fs.readFileSync('resources/views/login.edge');
-  const file = await octokit.repos.createOrUpdateFileContents({
-    owner: 'Germancitom',
-    repo: 'pruebas',
-    path: 'Hola',
-    message: 'mensaje de actualizacion',
-    content: buff.toString('base64'),
-    //sha: '...', // Asumiendo que tenemos el SHA del archivo existente (para actualizar)
-  });
     return view.render('welcome', {
       user: user || null
     })
-    
+  }
+  async upload({response}) {
+    const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN || '', // Usando process.env para acceder a la variable de entorno GITHUB_TOKEN
+    });
+
+    const buff = fs.readFileSync('mensaje');
+    const file = await octokit.repos.createOrUpdateFileContents({
+      owner: 'Germancitom',
+      repo: 'pruebas',
+      path: 'mensaje',
+      message: 'mensaje de actualizacion',
+      content: buff.toString('base64'),
+      //sha: '...', // Asumiendo que tenemos el SHA del archivo existente (para actualizar)
+    })
+    return response.json(file)
   }
 
 
